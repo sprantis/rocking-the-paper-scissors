229931f8050476d8c190159c1851f32f3386c89d
sprantis <sprantis@gmail.com>
2023-04-10 20:23:19 -0400

    added html template file and js psuedocode.

---
+++ script.js
@@ -0,0 +1,16 @@
+//Rock, paper, scrissors application that allows user to play against computer//
+
+//create a function that randomly picks between rock, paper, and scissors//
+
+//create a function that stores user input//
+
+//create a function that compares the user input vs computer input//
+
+//create a function provides user the result of the game//
+
+//create a function that stores the results of each game//
+
+//create a function that keeps track of the wins and losses//
+
+//create a function that gives option to restart//
+
